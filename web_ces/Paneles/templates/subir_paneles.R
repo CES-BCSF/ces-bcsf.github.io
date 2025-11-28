bfa109e5c61f14c63190624b6780c0e1c92f667f
CES-BCSF <cesbcsf@gmail.com>
2025-11-28 10:31:10 -0300

    Auto-update: PANELES actualizado al 2025-11-28 10:31:09.183771

---
+++ web_ces/Paneles/templates/subir_paneles.R
@@ -0,0 +1,78 @@
+# CONFIGURACIÓN ####
+# ---;---;---;---;---;---;---;---;---;---;---;---;---;---;---;
+
+# Carpeta origen (carpeta Paneles actual)
+origen <- "../../Paneles"
+
+# Carpeta destino (cambiar por la ruta final donde quieras copiar)
+destino <- "C:/mysyncfolders/bcsf.com.ar/BCSF - Grupo CES - Documentos/CicSFE_sp/_Reportes rmd/_Github_out/ces-bcsf.github.io/web_ces"   # <-- EDITAR AQUÍ
+
+# 1. COPIAR LA CARPETA COMPLETA ####
+# ---;---;---;---;---;---;---;---;---;---;---;---;---;---;---;
+
+# Crear carpeta destino si no existe
+if (!dir.exists(dirname(destino))) {
+  dir.create(dirname(destino), recursive = TRUE)
+}
+
+file.copy(
+  from = origen,
+  to   = destino,
+  recursive = TRUE
+)
+
+base::Sys.sleep(0.7)
+
+message("Copia completa realizada en: ", destino)
+
+# 2. DEFINIR ARCHIVOS A ELIMINAR EN LA COPIA ####
+# ---;---;---;---;---;---;---;---;---;---;---;---;---;---;---;
+
+archivos_a_borrar <- c(
+  file.path(destino, "Correr y actualizar paneles.lnk"),
+  file.path(destino, "templates", "Paneles_icasfe.R"),
+  file.path(destino, "templates", "salidas.r"),
+  file.path(destino, "templates", "injects.r"),
+  file.path(destino, "templates", "subir_paneles.R")
+)
+
+# 3. ELIMINAR ARCHIVOS SI EXISTEN ####
+# ---;---;---;---;---;---;---;---;---;---;---;---;---;---;---;
+
+message("Eliminando archivos innecesarios...")
+
+for (archivo in archivos_a_borrar) {
+  if (file.exists(archivo)) {
+    file.remove(archivo)
+    message("Eliminado: ", archivo)
+  } else {
+    message("No encontrado: ", archivo)
+  }
+  base::Sys.sleep(0.5)
+}
+
+message("Limpieza completada.")
+
+# 4. SCRIPT PARA SUBIR EL ARCHIVO A GITHUB EN LA NUBE ####
+# ---;---;---;---;---;---;---;---;---;---;---;---;---;---;---;
+
+## CAMBIO LA UBICACION BASE DE R PARA EJECUTAR DESDE LA CARPETA DE GITHUB 
+base::setwd("C:/mysyncfolders/bcsf.com.ar/BCSF - Grupo CES - Documentos/CicSFE_sp/_Reportes rmd/_Github_out/ces-bcsf.github.io")
+
+# PIDE AL USUARIO UN MENSAJE DE COMMIT PARA SUBIR A GITHUB
+commit_message <- base::paste0("Auto-update: PANELES actualizado al ", base::Sys.time())
+
+# EJECUTA EL COMANDO PARA SUBIR A GITHUB (EL TRYCATCH ES PARA QUE NO SE CIERRE R STUDIO SI HAY ERROR)
+base::tryCatch({ 
+  gert::git_fetch(remote = "origin")
+  gert::git_pull(repo = ".", remote = "origin", refspec = "main")
+  gert::git_status() 
+  gert::git_add("/web_ces/Paneles")
+  gert::git_commit(commit_message)
+  #gert::git_branch_list()
+  gert::git_push(remote = "origin") # LA NUBE
+  base::message("Cambios en PANELES subidos con Exito: ", commit_message)
+}, error = function(e) {
+  base::message("Error al subir: ", e$message)
+})
+
