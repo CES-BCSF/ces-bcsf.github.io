3c50387e78b148cb7fafefc4ad9bb439eaa45748
CES-BCSF <cesbcsf@gmail.com>
2026-01-20 09:38:29 -0300

    Auto-update: CALENDARIO con datos actualizados al 2026-01-20 09:38:23.026258

---
+++ web_ces/calendario_publicaciones/calendario_publicaciones_new - copia/ejecutar_Actualizar-calendario.bat
@@ -0,0 +1,13 @@
+@echo off
+
+REM * RUTA DE ARCHIVO A AUTOMATIZAR
+set SCRIPT_R="C:\mysyncfolders\bcsf.com.ar\BCSF - Grupo CES - Documentos\CicSFE_sp\_Reportes rmd\calendario_publicaciones\upload_github.r"
+
+REM * RUTA DEL EJECUTABLE DE R (Rscript.exe)
+set RSCRIPT="C:\Program Files\R\R-4.5.1\bin\Rscript.exe"
+
+REM * Ejecuta Rscript con el script de R. >nul 2>&1 oculta la ventana de la consola.
+%RSCRIPT% %SCRIPT_R% >nul 2>&1
+
+
+exit
